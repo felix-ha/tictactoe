afd36c42a119661669644eafce93a5476820f0f8
Felix Hauer <hauerf98@gmail.com>
2019-04-07 18:51:34 +0200

    created class, extracted blit method

--- gui.py
+++ gui.py
@@ -2,39 +2,47 @@ import pygame
 from pygame.locals import *
 
 
-class Object(pygame.sprite.Sprite):
+class Line(pygame.sprite.Sprite):
     def __init__(self, width, height):
-        super(Object, self).__init__()
+        super(Line, self).__init__()
         self.surf = pygame.Surface((width, height))
         self.surf.fill((0, 0, 0))
         self.rect = self.surf.get_rect()
 
-pygame.init()
 
-pygame.font.init()
-myfont = pygame.font.SysFont('Courier New', 120)
-textsurface = myfont.render('X', False, (0, 0, 0))
+class GUI:
+    def __init__(self):
+        pygame.init()
 
-line1 = Object(450, 5)
-line2 = Object(450, 5)
-line3 = Object(5, 450)
-line4 = Object(5, 450)
+        pygame.font.init()
+        myfont = pygame.font.SysFont('Courier New', 120)
+        textsurface = myfont.render('X', False, (0, 0, 0))
 
-screen = pygame.display.set_mode((450, 450))
-screen.fill((255, 255, 255))
+        self.lines = [Line(450, 5), Line(450, 5), Line(5, 450), Line(5, 450)]
+        self.line_positions = [(0, 150), (0, 300), (150, 0), (300, 0)]
 
-running = True
 
-while running:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
+        self.screen = pygame.display.set_mode((450, 450))
+        self.screen.fill((255, 255, 255))
 
-        screen.blit(textsurface, (35, 10))
+        running = True
 
-        screen.blit(line1.surf, (0, 150))
-        screen.blit(line2.surf, (0, 300))
-        screen.blit(line3.surf, (150, 0))
-        screen.blit(line4.surf, (300, 0))
-        pygame.display.flip()
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                self.screen.blit(textsurface, (35, 10))
+
+                self.blit_lines()
+
+                pygame.display.flip()
+
+    def blit_lines(self):
+        for i in range(4):
+            self.screen.blit(self.lines[i].surf, self.line_positions[i])
+
+
+if __name__ == '__main__':
+    gui = GUI()
