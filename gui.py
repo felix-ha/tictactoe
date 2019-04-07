7e6f172792d8023cdc4a404bc526e1f32dd6eb51
Felix Hauer <hauerf98@gmail.com>
2019-04-07 19:03:43 +0200

    wrote method to render game matrix

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import numpy as np
 
 
 class Line(pygame.sprite.Sprite):
@@ -14,9 +15,12 @@ class GUI:
     def __init__(self):
         pygame.init()
 
+        self.A = np.array([[1, -1, 1],
+                  [1, 1, 1],
+                  [-1, -1, -1]])
+
         pygame.font.init()
-        myfont = pygame.font.SysFont('Courier New', 120)
-        textsurface = myfont.render('X', False, (0, 0, 0))
+        self.myfont = pygame.font.SysFont('Courier New', 120)
 
         self.lines = [Line(450, 5), Line(450, 5), Line(5, 450), Line(5, 450)]
         self.line_positions = [(0, 150), (0, 300), (150, 0), (300, 0)]
@@ -33,15 +37,34 @@ class GUI:
                     if event.key == K_ESCAPE:
                         running = False
 
-                self.screen.blit(textsurface, (35, 10))
+                self.blit_game(self.A)
 
                 self.blit_lines()
-
                 pygame.display.flip()
 
     def blit_lines(self):
         for i in range(4):
             self.screen.blit(self.lines[i].surf, self.line_positions[i])
+
+    def blit_game(self, A):
+        # messed up coordinate convention of pygame
+        A = self.A.transpose()
+
+        height = width = 150
+
+        for i in range(3):
+            for j in range(3):
+                if A[i,j] == 0:
+                    continue
+                if A[i,j] == 1:
+                    letter = 'X'
+                if A[i,j] == -1:
+                    letter = 'O'
+
+                self.textsurface = self.myfont.render(letter, False, (0, 0, 0))
+                self.screen.blit(self.textsurface, (35 + i * height, 10 + j * width))
+
+
 
 
 if __name__ == '__main__':
